b2d6366ac29d63a723d1b1b8c80c572db5277f7d
salaudeen <olayiwolasalaudeen@gmail.com>
2024-01-02 08:01:09 +0100

    adding more founction

--- src/Component/MainWork.jsx
+++ src/Component/MainWork.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const MainWork = () => {
     const[advice, setAdvice] =useState("")
+    const [count, setCount] = useState(0)
 
     async function getAdvice() {
         const res = await fetch('https://api.adviceslip.com/advice')
         const data = await res.json()
         setAdvice(data.slip.advice)
+        setCount((c) => c + 1)
 
     }
+    useEffect(function(){
+       getAdvice()
+    }, [])
   return (
     <div>
         <h1>{advice}</h1>
         <button onClick={getAdvice}>Get advice</button>
+        <p>You have read <strong>{count}</strong> piece of advice from here</p>
     </div>
   )
 }
