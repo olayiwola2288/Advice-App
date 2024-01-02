c6aefc823e76ca15f7a3588632de1e3391cea194
salaudeen <olayiwolasalaudeen@gmail.com>
2024-01-02 09:42:47 +0100

    adding style

--- src/Component/MainWork.jsx
+++ src/Component/MainWork.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import img1 from "../assets/download.jfif"
 
 
 const MainWork = () => {
@@ -17,9 +18,20 @@ const MainWork = () => {
     }, [])
   return (
     <div>
-        <h1>{advice}</h1>
-        <button onClick={getAdvice}>Get advice</button>
-        <p>You have read <strong>{count}</strong> piece of advice from here</p>
+      <header className=" text-center">
+        <div>
+          <h1 className="text-green-900 font-bold text-2xl">Hello welcome 👏</h1>
+        <h1 className="text-lg mx-10">  Here is dev <strong className="text-green-900 text-xl  ">Olayiwola </strong> your all time developer </h1>
+        </div>
+        <div>
+          <img src={img1} alt="img" className="mx-auto rounded-[20px] my-5"  />
+        </div>
+      </header>
+      <p className="text-center mb-6 underline font-bold text-lg">Advice of the day</p>
+
+        <h1 className="text-center shadow mx-20 rounded-md p-10 bg-slate-100">{advice}</h1>
+        <button onClick={getAdvice} className="ms-8 mt-8 bg-slate-200 px-4 py-1 rounded-lg text-zinc-900 hover:bg-indigo-100">Get advice</button>
+        <p className="text-center my-10">You have read <strong className="text-green-900">{count}</strong> piece of advice from here</p>
     </div>
   )
 }
